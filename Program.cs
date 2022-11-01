3d4625ccfd46c6ef58e9a06350bf95df79d4a802
Anton <anton@mail.ru>
2022-11-01 12:33:15 +0400

    прием всех  массива

--- Program.cs
+++ Program.cs
@@ -3,5 +3,19 @@ using static System.Console;
 
 
 
+string[] GenerateArray(int length){
+    var array = new string[length];
+    for (int i = 0; i < length; i++)
+    { 
+        System.Console.Write("Элемент массива: ");
+        array[i] = Convert.ToString(Console.ReadLine());
+    }
+return array;
+}
+
+
+
+
+
 System.Console.Write("Введите количество элементов массива: ");
 int num = Convert.ToInt32(Console.ReadLine());
